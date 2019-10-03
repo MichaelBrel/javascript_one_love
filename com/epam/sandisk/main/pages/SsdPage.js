d020cb9da698acbbf364ed91028caf2d61f924e0
Michael Brel <brel.mihail@mail.ru>
2019-10-03 14:40:29 +0300

    Add methods and field to SsdPage.js

--- com/epam/sandisk/main/pages/SsdPage.js
+++ com/epam/sandisk/main/pages/SsdPage.js
@@ -1,13 +1,46 @@
+const Wait = require('../util/Wait');
+let wait = new Wait();
+const Actions = require('../util/Actions');
+let actions = new Actions();
+const ElementArrayFinderHandler = require('../util/ElementArrayFinderHandler');
+let elementArrayFinderHandler = new ElementArrayFinderHandler();
+const ArrayHandler = require('../util/ArrayHandler');
+let arrayHandler = new ArrayHandler();
+
 class SsdPage {
     constructor() {
-        this.productTitles = element(by.xpath("//*[@ng-bind-html='product.title']"));
+        this.productTitles = element.all(by.css("[ng-bind-html='product.title']"));
+        this.clearButton = element(by.css(".hide-below2 a.btn"));
+        this.mSataRadioButton = element(by.xpath("//label//*[contains(text(),'mSATA')]"));
+        this.welcomeHeader = element(by.xpath("//h1[contains(text(), 'These Are the Drives')]"));
+        this.allRadioButtons = element.all(by.css(".filter-panel.ng-scope input"));
     }
 
-    productTitleContainsText(text) {
-        return this.productTitles
-            .getText()
-            .then((x) => x.includes(text));
+    allProductTitlesContainString(string) {
+        return elementArrayFinderHandler.getTextArray(this.productTitles)
+            .then((arr) => {
+                return arrayHandler.allElementsContainString(arr, string)
+            });
     }
+
+    clickMsataRadioButton() {
+        return actions.moveAndClick(this.mSataRadioButton);
+    }
+
+    clickClearButton() {
+        return this.clearButton.click();
+    }
+
+    allRadioButtonsPressed() {
+        return elementArrayFinderHandler.getAttributes(this.allRadioButtons, "class")
+            .then((arr) => {
+                return arrayHandler.filterByString(arr, "ng-valid ng-dirty ng-touched ng-valid-parse")
+            })
+            .then((arr) => {
+                return arr.length > 0
+            });
+    }
+
 }
 
-module.exports = new SsdPage();
+module.exports = SsdPage;
